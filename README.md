7c8ae2d3b8991d38b5b80a1a8ee4d5aa3584ed8f
Metin Oktay DENİZ <metinoktaydeniz@hotmail.com>
2024-04-26 10:06:00 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>STOPWORDS FOR TURKISH LANGUAGE</h1>
+<h3>import stopwordstr</h3></h3>
+<h3>stopwordstr.stopwords()</h3>
---
+++ stopwordstr.py
@@ -0,0 +1,9 @@
+import os
+
+def stopwords():
+    txt=open("turkish.txt", "r")
+    stopwords=[]
+    for i in txt:
+        i=i[0:-1]
+        stopwords.append(i)
+    return stopwords
